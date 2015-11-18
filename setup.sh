696200bda08e1764b6a8e9f2c9a17bd7c4a6b31e
Ivan Georgiev <ivan.georgiev@gmail.com>
2015-11-18 21:29:00 +0200

    Remove unnecessary highlight.js files

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#npm install -g --upgrade npm
+#npm install -g --upgrade docpad
+
+npm install
+docpad install
+git clone https://ivangeorgiev@github.com/ivangeorgiev/igeorgiev.git out
+cd out
+git checkout gh-pages
