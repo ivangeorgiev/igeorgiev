35c54c5964b01c835aacabcb89bf5e5800fc12ed
IvanGeorgiev <ivan.georgiev@gmail.com>
2015-12-01 14:49:35 +0200

    Working on ngsocial

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!bash
+
+cd  /c/Data/Sandbox/Angular/Angular10Prj/Lesson_05/igeorgiev
+rm -rf ngsocial
+mkdir ngsocial
+cp -R ../ngsocial/app/* ngsocial
+
+git add . --all
+git commit -m "Working on ngsocial"
+
